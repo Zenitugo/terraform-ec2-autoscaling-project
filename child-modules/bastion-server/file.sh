172e3bf98a0d0582cb809e1774c8c45af6c878af
Ugochi Ukaegbu <ugochiukaegbu19@gmail.com>
2024-01-30 20:48:47 +0100

    A script contaiing the code to be inputted in the user data section of the bastion ec2

--- child-modules/bastion-server/file.sh
+++ child-modules/bastion-server/file.sh
@@ -10,3 +10,6 @@ echo "Installing Ansible..."
 apt-get install -y software-properties-common python-apt-common
 apt install -y ansible
 
+ec2_public_key=$(ssh-keygen -y -f ./terraform-autoscale/child-modules/bastion-server/key-folder/my-keys.pem)
+
+echo '$ec2_public_key' > ./terraform-autoscale/child-modules/bastion-server/key-folder/my-keys.pub

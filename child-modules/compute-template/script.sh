5c4f58b533e7b6deb9c388c5e0c7971cb53a2d9f
Ugochi Ukaegbu <110994907+Zenitugo@users.noreply.github.com>
2024-01-30 21:21:15 +0100

    script used in the user data section of the  ec2 template

--- child-modules/compute-template/script.sh
+++ child-modules/compute-template/script.sh
@@ -4,5 +4,8 @@ mkdir -p /home/ubuntu/html
 cd /home/ubuntu/html
 touch /home/ubuntu/html/index.html.j2
 
+############################################################################################################
+#############################################################################################################
+
 hostnames=$(hostname)
 echo "<h1>Welcome to $hostnames server</h1>" > /home/ubuntu/html/index.html.j2

349dc678794b835e37cbd2e5af6a0095edd85ccf
Ugochi Ukaegbu <110994907+Zenitugo@users.noreply.github.com>
2024-01-30 20:57:42 +0100

    The file outputting the values such as id of the infrastructure created in the main.tf

--- child-modules/auto-scaling/output.tf
+++ child-modules/auto-scaling/output.tf
@@ -1,9 +1,11 @@
-# output the id of the autoscaling group
+# Output the id of the autoscaling group
 
 output "autoscaling" {
     value = aws_autoscaling_group.ec2-autoscale.id
 }
 
+
+# Output the name of the autoscale group
 output "scaling" {
   value = aws_autoscaling_group.ec2-autoscale.*.name
 }
